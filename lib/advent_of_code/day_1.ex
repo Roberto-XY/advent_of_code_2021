4c8244c8c14a7926e0fe76e5fbded681b11cc541
roberto <roberto.fischer@smartclip.tv>
2021-12-01 14:55:12 +0100

    Solve day1

--- lib/advent_of_code/day_1.ex
+++ lib/advent_of_code/day_1.ex
@@ -1,18 +1,32 @@
 defmodule Day1 do
-  @moduledoc """
-  Documentation for `AdventOfCode`.
-  """
+  @example_input [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-  @doc """
-  Hello world.
+  @spec read_input! :: list(integer())
+  def read_input!() do
+    File.read!(Path.join(:code.priv_dir(:advent_of_code), "input/day_1.txt"))
+    |> String.split("\n")
+    |> Enum.map(fn x ->
+      {res, ""} = Integer.parse(x)
+      res
+    end)
+  end
 
-  ## Examples
+  @spec solve_1(list(integer())) :: integer()
+  def solve_1(list \\ @example_input) do
+    Enum.chunk_every(list, 2, 1, :discard)
+    |> Enum.reduce(0, fn [first, second], acc ->
+      if first < second do
+        acc + 1
+      else
+        acc
+      end
+    end)
+  end
 
-      iex> AdventOfCode.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec solve_2(list(integer())) :: integer()
+  def solve_2(list \\ @example_input) do
+    Enum.chunk_every(list, 3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> solve_1()
   end
 end
