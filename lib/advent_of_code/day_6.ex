b679b8637e69044cd21dea772765ff8bdc4635c0
roberto <roberto.fischer@smartclip.tv>
2021-12-06 11:47:54 +0100

    day6 not brain dead

--- lib/advent_of_code/day_6.ex
+++ lib/advent_of_code/day_6.ex
@@ -1,6 +1,8 @@
 defmodule Day6 do
   @example_input [3, 4, 3, 1, 2]
 
+  @type timers :: %{optional(1..8) => non_neg_integer()}
+
   @spec solve! :: String.t()
   def solve!() do
     read_input!()
@@ -18,27 +20,43 @@ defmodule Day6 do
     |> Enum.map(&String.to_integer/1)
   end
 
-  @spec solve_1(Enumerable.t(), non_neg_integer()) :: integer()
-  def solve_1(enum \\ @example_input, days \\ 80)
-
-  def solve_1(enum, 0) when is_list(enum) do
-    length(enum)
+  @spec solve_1(Enumerable.t()) :: non_neg_integer()
+  def solve_1(enum \\ @example_input) do
+    Enum.reduce(enum, %{}, fn timer, acc ->
+      Map.update(acc, timer, 1, &(&1 + 1))
+    end)
+    |> do_solve_1()
   end
 
-  def solve_1(enum, days) when is_list(enum) and is_integer(days) and days >= 0 do
-    solve_1(update_day(enum), days - 1)
+  @spec do_solve_1(timers(), non_neg_integer()) ::
+          non_neg_integer()
+  def do_solve_1(timers, days \\ 80)
+
+  def do_solve_1(timers, 0) when is_map(timers) do
+    Map.values(timers) |> Enum.sum()
   end
 
-  @spec update_day([non_neg_integer()]) :: [non_neg_integer()]
-  def update_day(counters) do
-    Enum.flat_map(counters, fn
-      0 -> [6, 8]
-      counter -> [counter - 1]
+  def do_solve_1(timers, days) when is_map(timers) and is_integer(days) and days >= 0 do
+    do_solve_1(update_day(timers), days - 1)
+  end
+
+  @spec update_day(timers()) :: timers()
+  def update_day(timers) when is_map(timers) do
+    Enum.reduce(timers, %{}, fn
+      {0, count}, acc ->
+        Map.update(acc, 6, count, &(&1 + count))
+        |> Map.update(8, count, &(&1 + count))
+
+      {timer, count}, acc ->
+        Map.update(acc, timer - 1, count, &(&1 + count))
     end)
   end
 
   @spec solve_2(Enumerable.t()) :: integer()
   def solve_2(enum \\ @example_input) do
-    solve_1(enum, 256)
+    Enum.reduce(enum, %{}, fn timer, acc ->
+      Map.update(acc, timer, 1, &(&1 + 1))
+    end)
+    |> do_solve_1(256)
   end
 end
