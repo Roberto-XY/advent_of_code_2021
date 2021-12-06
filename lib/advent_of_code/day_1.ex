841d8f2e08887b31ef925d32cde5965d6ae39e9c
roberto <roberto.fischer@smartclip.tv>
2021-12-06 11:57:51 +0100

    smarter day1

--- lib/advent_of_code/day_1.ex
+++ lib/advent_of_code/day_1.ex
@@ -1,9 +1,19 @@
 defmodule Day1 do
   @example_input [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
+  @spec solve! :: String.t()
+  def solve!() do
+    read_input!()
+    |> Enum.to_list()
+    |> then(&%{res_1: solve_1(&1), res_2: solve_2(&1)})
+    |> then(fn
+      %{res_1: 1451, res_2: 1395} -> IO.inspect("Success on #{__MODULE__}")
+    end)
+  end
+
   @spec read_input! :: Enumerable.t()
   def read_input!() do
-    File.stream!(Path.join(:code.priv_dir(:advent_of_code), "input/day_1.txt"))
+    File.stream!(Path.join(:code.priv_dir(:advent_of_code), "input/day_6.txt"))
     |> Stream.map(&String.to_integer(String.trim(&1)))
   end
 
@@ -16,7 +26,7 @@ defmodule Day1 do
   @spec solve_2(Enumerable.t()) :: integer()
   def solve_2(enum \\ @example_input) do
     Stream.chunk_every(enum, 3, 1, :discard)
-    |> Stream.map(&Enum.sum/1)
-    |> solve_1()
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [[left, _, _], [_, _, right]] -> right > left end)
   end
 end
