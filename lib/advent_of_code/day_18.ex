c8aa8607c601bfc64e509741c5575fce00e1f789
roberto <roberto.fischer@smartclip.tv>
2021-12-20 03:16:36 +0100

    x

--- lib/advent_of_code/day_18.ex
+++ lib/advent_of_code/day_18.ex
@@ -5,7 +5,7 @@ defmodule Day18 do
     |> Stream.map(&parse_input_line(String.trim(&1)))
     |> then(&%{res_1: solve_1(&1), res_2: solve_2(&1)})
     |> then(fn
-      %{res_1: 10878, res_2: 4716} -> IO.inspect("Success on #{__MODULE__}")
+      %{res_1: 3691, res_2: 4756} -> IO.inspect("Success on #{__MODULE__}")
     end)
   end
 
@@ -18,126 +18,134 @@ defmodule Day18 do
     number
   end
 
-  def do_parse(binary, depth \\ -1)
-
-  def do_parse(<<"[", rest::binary>>, depth) do
-    {left, <<",", rest::binary>>} = do_parse(rest, depth + 1)
-    {right, <<"]", rest::binary>>} = do_parse(rest, depth + 1)
+  def do_parse(<<"[", rest::binary>>) do
+    {left, <<",", rest::binary>>} = do_parse(rest)
+    {right, <<"]", rest::binary>>} = do_parse(rest)
     {[left, right], rest}
   end
 
-  def do_parse(<<x::binary-size(1), rest::binary>>, depth) do
-    {%{value: String.to_integer(x), depth: depth}, rest}
+  def do_parse(<<x::binary-size(1), rest::binary>>) do
+    {String.to_integer(x), rest}
   end
 
   def solve_1(numbers) do
     numbers
-    |> Stream.map(fn number ->
-      List.flatten(number)
-      |> Stream.with_index()
-      |> Stream.map(fn {a, b} -> {b, a} end)
-      |> Enum.into(%{})
-    end)
     |> Enum.reduce(&add(&2, &1))
+    |> magnitude()
   end
 
-  def add(a, b) do
-    a =
-      Stream.map(a, fn {_, group} -> %{group | depth: group.depth + 1} end)
-      |> Enum.to_list()
-      |> IO.inspect(label: :A)
-
-    b =
-      Stream.map(b, fn {_, group} -> %{group | depth: group.depth + 1} end)
-      |> Enum.to_list()
-      |> IO.inspect(label: :B)
-
-    new_number =
-      Stream.concat(a, b)
-      |> Stream.with_index()
-      |> Stream.map(fn {a, b} -> {b, a} end)
-      |> Enum.into(%{})
-
-    new_number |> IO.inspect(label: :new_number)
-
-    Enum.reduce_while(
-      Stream.cycle([0]),
-      new_number,
-      fn
-        _, last_number ->
-          current_number =
-            Enum.reduce_while(last_number, last_number, fn
-              {index, %{depth: depth}}, acc when depth >= 4 ->
-                {:halt, explode_at(acc, index)}
-                |> IO.inspect(label: "explode_at#{index}")
-
-              {index, %{value: value}}, acc when value >= 10 ->
-                {:halt, split_at(acc, index)}
-                |> IO.inspect(label: "split_at#{index}")
-
-              _, acc ->
-                {:cont, acc}
-            end)
-
-          if current_number == last_number do
-            {:halt, current_number}
-          else
-            {:cont, current_number}
-          end
-      end
-    )
+  def solve_2(numbers) do
+    for number1 <- numbers,
+        number2 <- numbers,
+        number1 != number2 do
+      {number1, number2}
+    end
+    |> Stream.map(fn {number1, number2} -> add(number1, number2) end)
+    |> Stream.map(&magnitude/1)
+    |> Enum.max()
   end
 
-  def explode_at(number, index) when is_map(number) do
-    # IO.inspect({index, number}, label: :explode_at_INNER)
-    first = Map.fetch!(number, index)
-    second = Map.fetch!(number, index + 1)
+  @type snail_number_component :: [non_neg_integer()]
 
-    if first.depth >= 4 and second.depth >= 4 do
-      update_existing(number, index - 1, &%{depth: &1.depth, value: &1.value + first.value})
-      |> update_existing(index + 2, &%{depth: &1.depth, value: &1.value + second.value})
-      |> Map.drop([index])
-      |> Enum.map(fn
-        {i, num} when i > index -> {i - 1, num}
-        i_num -> i_num
-      end)
-      |> Enum.into(%{})
-      |> Map.update!(index, fn _ -> %{depth: first.depth - 1, value: 0} end)
-    else
-      number
+  def magnitude([left, right]) do
+    3 * magnitude(left) + 2 * magnitude(right)
+  end
+
+  def magnitude(leaf) when is_integer(leaf) do
+    leaf
+  end
+
+  def add(number1, number2) do
+    reduce([number1, number2])
+  end
+
+  def reduce(number) do
+    case explode(number, 0) do
+      %{exploded: true, new_value: number} ->
+        reduce(number)
+
+      _ ->
+        case split(number) do
+          %{split: true, new_value: number} ->
+            reduce(number)
+
+          _ ->
+            number
+        end
     end
   end
 
-  def split_at(number, index) do
-    # IO.inspect({index, number}, label: :split_at_INNER)
-
-    elem = Map.fetch!(number, index)
-
-    if elem.value >= 10 do
-      Enum.map(number, fn
-        {i, num} when i > index -> {i + 1, num}
-        i_num -> i_num
-      end)
-      |> Enum.into(%{})
-      |> Map.put(index, %{depth: elem.depth + 1, value: floor(elem.value / 2)})
-      |> Map.put(index + 1, %{depth: elem.depth + 1, value: ceil(elem.value / 2)})
-    else
-      number
-    end
-
-    #     def split(arr, ind)
-    #   arr.insert(ind + 1, (arr[ind].real / 2.0).round + (arr[ind].imaginary + 1).i)
-    #   arr[ind] = arr[ind].real / 2 + (arr[ind].imaginary + 1).i
-    # end
+  def explode([left, right], 4) do
+    %{exploded: true, new_value: 0, left_add: left, right_add: right}
   end
 
-  def update_existing(map, key, fun) do
-    case map do
-      %{^key => old} -> %{map | key => fun.(old)}
-      %{} -> map
+  def explode([left, right], depth) do
+    case explode(left, depth + 1) do
+      %{exploded: true, new_value: new_value, left_add: left_add, right_add: right_add} ->
+        %{
+          exploded: true,
+          new_value: [new_value, add_left(right, right_add)],
+          left_add: left_add,
+          right_add: 0
+        }
+
+      %{exploded: false} ->
+        case explode(right, depth + 1) do
+          %{exploded: true, new_value: new_value, left_add: left_add, right_add: right_add} ->
+            %{
+              exploded: true,
+              new_value: [add_right(left, left_add), new_value],
+              left_add: 0,
+              right_add: right_add
+            }
+
+          %{exploded: false} ->
+            %{exploded: false}
+        end
     end
   end
 
-  def solve_2(x) do
+  def explode(leaf, _) when is_integer(leaf) do
+    %{exploded: false}
+  end
+
+  def add_left([left, right], value) when is_integer(value) do
+    [add_left(left, value), right]
+  end
+
+  def add_left(leaf, value) when is_integer(leaf) and is_integer(value) do
+    leaf + value
+  end
+
+  def add_right([left, right], value) when is_integer(value) do
+    [left, add_right(right, value)]
+  end
+
+  def add_right(leaf, value) when is_integer(leaf) and is_integer(value) do
+    leaf + value
+  end
+
+  def split([left, right]) do
+    case split(left) do
+      %{split: true, new_value: new_left_value} ->
+        %{split: true, new_value: [new_left_value, right]}
+
+      %{split: false} ->
+        case split(right) do
+          %{split: true, new_value: new_right_value} ->
+            %{split: true, new_value: [left, new_right_value]}
+
+          %{split: false} ->
+            %{split: false}
+        end
+    end
+  end
+
+  def split(leaf) when is_integer(leaf) and leaf >= 10 do
+    %{split: true, new_value: [floor(leaf / 2), ceil(leaf / 2)]}
+  end
+
+  def split(leaf) when is_integer(leaf) do
+    %{split: false}
   end
 end
