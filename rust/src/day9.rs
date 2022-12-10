2110b9ba806a2ea4ba84d47f6d86cde6252f1177
Roberto Fischer <a.robertofischer@gmail.com>
2022-12-10 08:20:29 +0100

    ADD day 9 2nd part

--- rust/src/day9.rs
+++ rust/src/day9.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     env,
     error::Error,
     fs::File,
@@ -33,59 +33,109 @@ fn main() -> Result<(), Box<dyn Error>> {
     let path = env::current_dir()?.join("data/day9.txt");
     let reader = BufReader::new(File::open(path.clone())?);
 
-    let mut head_pos = (0, 0);
-    let mut tail_pos = (0, 0);
-    let mut pos_visited_by_tail: HashSet<(i32, i32)> = HashSet::new();
-    pos_visited_by_tail.insert(tail_pos);
+    let mut rope = [(0, 0); 10];
+    let mut pos_visited_by_knots: HashMap<usize, HashSet<(i32, i32)>> = HashMap::new();
 
     for line_or_err in reader.lines() {
         let line = line_or_err?;
-
         let mut instruction_segments = line.split_whitespace();
-        let direction: Direction = instruction_segments.next().ok_or("err")?.try_into()?;
-        let repetition = instruction_segments.next().ok_or("err")?.parse::<usize>()?;
-        iter::repeat(&direction)
-            .take(repetition)
-            .for_each(|direction| match direction {
+        let direction: Direction = instruction_segments
+            .next()
+            .ok_or("parsing error")?
+            .try_into()?;
+        let repetition = instruction_segments
+            .next()
+            .ok_or("parsing error")?
+            .parse::<usize>()?;
+
+        for direction in iter::repeat(&direction).take(repetition) {
+            match direction {
                 Direction::Up => {
-                    head_pos.1 += 1;
-                    if must_tail_move(head_pos, tail_pos) {
-                        tail_pos.0 = head_pos.0;
-                        tail_pos.1 = head_pos.1 - 1;
-                        pos_visited_by_tail.insert(tail_pos);
-                    }
+                    rope[0].1 += 1;
                 }
                 Direction::Down => {
-                    head_pos.1 -= 1;
-                    if must_tail_move(head_pos, tail_pos) {
-                        tail_pos.0 = head_pos.0;
-                        tail_pos.1 = head_pos.1 + 1;
-                        pos_visited_by_tail.insert(tail_pos);
-                    }
+                    rope[0].1 -= 1;
                 }
                 Direction::Right => {
-                    head_pos.0 += 1;
-                    if must_tail_move(head_pos, tail_pos) {
-                        tail_pos.0 = head_pos.0 - 1;
-                        tail_pos.1 = head_pos.1;
-                        pos_visited_by_tail.insert(tail_pos);
-                    }
+                    rope[0].0 += 1;
                 }
                 Direction::Left => {
-                    head_pos.0 -= 1;
-                    if must_tail_move(head_pos, tail_pos) {
-                        tail_pos.0 = head_pos.0 + 1;
-                        tail_pos.1 = head_pos.1;
-                        pos_visited_by_tail.insert(tail_pos);
-                    }
+                    rope[0].0 -= 1;
                 }
-            });
+            };
+
+            for idx in 1..rope.len() {
+                let head_pos = rope[idx - 1];
+                let next_tail_pos = rope.get_mut(idx).ok_or("idx out of bounds")?;
+
+                // top-left corner
+                if (next_tail_pos.0 - 1, next_tail_pos.1 + 2) == head_pos
+                    || (next_tail_pos.0 - 2, next_tail_pos.1 + 1) == head_pos
+                    || (next_tail_pos.0 - 2, next_tail_pos.1 + 2) == head_pos
+                {
+                    next_tail_pos.0 = next_tail_pos.0 - 1;
+                    next_tail_pos.1 = next_tail_pos.1 + 1;
+                }
+                // left lane
+                else if (next_tail_pos.0 - 2, next_tail_pos.1) == head_pos {
+                    next_tail_pos.0 = next_tail_pos.0 - 1;
+                }
+                // bottom-left corner
+                else if (next_tail_pos.0 - 2, next_tail_pos.1 - 1) == head_pos
+                    || (next_tail_pos.0 - 1, next_tail_pos.1 - 2) == head_pos
+                    || (next_tail_pos.0 - 2, next_tail_pos.1 - 2) == head_pos
+                {
+                    next_tail_pos.0 = next_tail_pos.0 - 1;
+                    next_tail_pos.1 = next_tail_pos.1 - 1;
+                }
+                // bottom lane
+                else if (next_tail_pos.0, next_tail_pos.1 - 2) == head_pos {
+                    next_tail_pos.1 = next_tail_pos.1 - 1;
+                }
+                // bottom-right corner
+                else if (next_tail_pos.0 + 1, next_tail_pos.1 - 2) == head_pos
+                    || (next_tail_pos.0 + 2, next_tail_pos.1 - 1) == head_pos
+                    || (next_tail_pos.0 + 2, next_tail_pos.1 - 2) == head_pos
+                {
+                    next_tail_pos.0 = next_tail_pos.0 + 1;
+                    next_tail_pos.1 = next_tail_pos.1 - 1;
+                }
+                // right lane
+                else if (next_tail_pos.0 + 2, next_tail_pos.1) == head_pos {
+                    next_tail_pos.0 = next_tail_pos.0 + 1;
+                }
+                // top-right corner
+                else if (next_tail_pos.0 + 2, next_tail_pos.1 + 1) == head_pos
+                    || (next_tail_pos.0 + 1, next_tail_pos.1 + 2) == head_pos
+                    || (next_tail_pos.0 + 2, next_tail_pos.1 + 2) == head_pos
+                {
+                    next_tail_pos.0 = next_tail_pos.0 + 1;
+                    next_tail_pos.1 = next_tail_pos.1 + 1;
+                }
+                // top lane
+                else if (next_tail_pos.0, next_tail_pos.1 + 2) == head_pos {
+                    next_tail_pos.1 = next_tail_pos.1 + 1;
+                }
+
+                pos_visited_by_knots
+                    .entry(idx)
+                    .and_modify(|set| {
+                        set.insert(*next_tail_pos);
+                    })
+                    .or_insert_with(|| {
+                        let mut set = HashSet::new();
+                        set.insert((0, 0));
+                        set
+                    });
+            }
+        }
     }
 
-    dbg!(pos_visited_by_tail.len());
-    Ok(())
-}
+    let res_1 = pos_visited_by_knots.get(&1).map(|set| set.len());
+    let res_2 = pos_visited_by_knots.get(&9).map(|set| set.len());
 
-fn must_tail_move(head_pos: (i32, i32), tail_pos: (i32, i32)) -> bool {
-    head_pos.0.abs_diff(tail_pos.0) > 1 || head_pos.1.abs_diff(tail_pos.1) > 1
+    dbg!(res_1);
+    dbg!(res_2);
+
+    Ok(())
 }
